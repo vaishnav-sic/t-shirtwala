2abcc2440298227cfede498bf823ae2a9a5436f6
rushi5059 <rushi5059@gmail.com>
2024-03-13 23:44:33 +0530

    conflict resolve

--- src/components/index.js
+++ src/components/index.js
@@ -6,4 +6,5 @@
 
 export {default as NavBar} from "./NavBar"
 export {default as Categories} from "./Categories"
-export {default as Footer} from "./Fotter"
+export {default as GetMore} from "./GetMore"
+export {default as Footer} from "./Footer"
